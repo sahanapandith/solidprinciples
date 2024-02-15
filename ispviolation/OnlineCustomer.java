2036358d0b979d006d946913f8ba09dc4e53e5e2
sahanapandith <98015559+sahanapandith@users.noreply.github.com>
2024-02-15 13:03:20 +0530

    Update OnlineCustomer.java

--- ispviolation/OnlineCustomer.java
+++ ispviolation/OnlineCustomer.java
@@ -2,7 +2,7 @@ package ispviolation;
 
 public class OnlineCustomer implements OnlineTransaction {
     public void bankApp() {
-        System.out.println("pay through bank appn");
+        System.out.println("pay through bank application");
     }
 
     public void payPal() {
